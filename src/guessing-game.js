8b617fd2b6d96f7410a9ec3b2e175a91b34d0cc8
Трус Роман < >
2017-02-05 21:53:58 +0300

    nothing to commit. made task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min=0;
+    	this.max=0;
+    	
+    }
 
     setRange(min, max) {
+    	this.min=min;
+    	this.max=max;
 
     }
 
     guess() {
-
+    		return(Math.ceil(this.min+((this.max-this.min)/2)));
+    
     }
 
     lower() {
-
-    }
+    	this.max=Math.ceil(this.min+((this.max-this.min)/2));
+    } 
+   
 
     greater() {
+    	this.min=Math.ceil(this.min+((this.max-this.min)/2));
 
     }
 }
